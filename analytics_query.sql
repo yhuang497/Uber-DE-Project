c6c1315bb75712fa03e3ecca5c1e98f277db7a6f
yhuang497 <141279609+yhuang497@users.noreply.github.com>
2024-05-01 22:49:03 +0800

    Add files via upload

---
+++ analytics_query.sql
@@ -0,0 +1,68 @@
+-- create updated fact_table_1
+CREATE TABLE `circular-music-421713.uber_data_engineering.fact_table_1` AS (
+  WITH 
+  numbered_fact_table AS (
+    SELECT
+      *,
+      ROW_NUMBER() OVER () AS fact_row_number
+    FROM
+      `circular-music-421713.uber_data_engineering.fact_table`
+  ),
+  numbered_dropoff_location_dim AS (
+    SELECT
+      *,
+      ROW_NUMBER() OVER () AS dropoff_row_number
+    FROM
+      `circular-music-421713.uber_data_engineering.dropoff_location_dim`
+  )
+  SELECT
+    f.*,
+    d.dropoff_location_id
+  FROM
+    numbered_fact_table AS f
+  JOIN
+    numbered_dropoff_location_dim AS d
+  ON
+    f.fact_row_number = d.dropoff_row_number
+
+)
+
+-- create table for analytics
+CREATE OR REPLACE TABLE `circular-music-421713.uber_data_engineering.tbl_analytics` AS (
+  SELECT 
+    f.trip_id,
+    f.VendorID,
+    d.tpep_pickup_datetime,
+    d.tpep_dropoff_datetime,
+    p.passenger_count,
+    t.trip_distance,
+    r.rate_code_name,
+    pick.pickup_latitude,
+    pick.pickup_longitude,
+    drop.dropoff_latitude,
+    drop.dropoff_longitude,
+    pay.payment_type_name,
+    f.fare_amount,
+    f.extra,
+    f.mta_tax,
+    f.tip_amount,
+    f.tolls_amount,
+    f.improvement_surcharge,
+    f.total_amount
+
+  FROM 
+    `circular-music-421713.uber_data_engineering.fact_table_1` f
+    JOIN `circular-music-421713.uber_data_engineering.datetime_dim` d  
+      ON f.datetime_id=d.datetime_id
+    JOIN `circular-music-421713.uber_data_engineering.passenger_count_dim` p  
+      ON p.passenger_count_id=f.passenger_count_id  
+    JOIN `circular-music-421713.uber_data_engineering.trip_distance_dim` t  
+      ON t.trip_distance_id=f.trip_distance_id
+    JOIN `circular-music-421713.uber_data_engineering.rate_code_dim` r 
+      ON r.rate_code_id=f.rate_code_id  
+    JOIN `circular-music-421713.uber_data_engineering.pickup_location_dim` pick 
+      ON pick.pickup_location_id=f.pickup_location_id
+    JOIN `circular-music-421713.uber_data_engineering.dropoff_location_dim` drop 
+      ON drop.dropoff_location_id=f.dropoff_location_id
+    JOIN `circular-music-421713.uber_data_engineering.payment_type_dim` pay 
+      ON pay.payment_type_id=f.payment_type_id);
